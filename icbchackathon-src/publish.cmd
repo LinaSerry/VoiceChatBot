f702738b1f2de23acb6ec6db98cfbe2e498e5109
Lina Serry <liserry@microsoft.com>
2019-01-15 10:34:08 -0800

    initializing project

---
+++ icbchackathon-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=icbchackathon-Web-Deploy.pubxml -p:Password=F4y6QR08pLKhqZhBzTXFDvRbtxNiynQkZxpcxJHW2KuYo3eTBpscll3YPKwk
+
